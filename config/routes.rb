ee84f6e80e85b2e7925df69a495829ce40c18ffd
A23I005 <A23I005hn@ous.jp>
2025-10-20 20:19:54 +0900

    コメント機能を追加：モデル、コントローラー、ビュー、ルーティングを実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resource :like, only: [:create, :destroy]
+    resources :comments, only: [:create]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
